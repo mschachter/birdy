6e8e06fff187393b2fc5f4be9b5991e2ecd3ea30
mschachter <mike@mindmech.com>
2013-07-23 14:00:54 -0700

    working on getting motograms to work...

--- src/python/motograms.py
+++ src/python/motograms.py
@@ -1,8 +1,12 @@
 import os
 import h5py
 import numpy as np
-from tools.signal import gaussian_stft
 
+from scipy.interpolate import interp1d
+
+import matplotlib.pyplot as plt
+
+from tools.signal import gaussian_stft
 from tools.sound import log_spectrogram,WavFile
 
 from oscillators import NormalOscillator
@@ -13,7 +17,7 @@ class Motogram(object):
     def __init__(self):
         self.oscillator = NormalOscillator()
         self.sample_rate = 1000.0
-        self.waveform_sample_rate = 1e6
+        self.waveform_sample_rate = 1e5
         self.alpha = list()
         self.beta = list()
         self.mu = list()
@@ -34,31 +38,43 @@ class Motogram(object):
         waveform = np.zeros(Nwave)
         print 'nsteps=%d, total_duration=%0.6f, step_duration=%0.6f, dt=%0.6f, Nwave=%d' % (nsteps, total_duration, step_duration, dt, Nwave)
 
+        #extend alpha and beta by one time step for interpolation
+        alpha_ext = np.zeros(nsteps+1)
+        alpha_ext[:-1] = self.alpha
+        alpha_ext[-1] = alpha_ext[-2]
+        
+        beta_ext = np.zeros(nsteps+1)
+        beta_ext[:-1] = self.beta
+        beta_ext[-1] = beta_ext[-2]
+
+        #upsample alpha and beta to a finer timescale for simulation
+        t_alpha = np.arange(nsteps+1)*(1.0 / self.sample_rate)
+        f_alpha = interp1d(t_alpha, alpha_ext)
+        f_beta = interp1d(t_alpha, beta_ext)
+
         #simulate the oscillator to produce the sound pressure waveform
-        steps_per_step = int(step_duration / dt)
-        last_index = 0
         last_x = 0.0
         last_v = 0.0
-        for k in range(nsteps):
-            states = self.oscillator.simulate(last_x, last_v, step_duration, dt, alpha=self.alpha[k], beta=self.beta[k])
-            print 'k=%d, last_x=%f, last_v=%f, step_duration=%0.6f, dt=%0.6f, alpha=%0.6f, beta=%0.6f' % (k, last_x, last_v, step_duration, dt, self.alpha[k], self.beta[k])
-            print 'states.shape=',states.shape
-            si = last_index
-            ei = last_index + steps_per_step
-            waveform[si:ei] = states[:steps_per_step, 0]
+        for k in range(Nwave):
+            t = k*dt
+            a = f_alpha(t)
+            b = f_beta(t)
+            states = self.oscillator.simulate(last_x, last_v, dt, dt, alpha=a, beta=b)
+            #print 'k=%d, last_x=%f, last_v=%f, step_duration=%0.6f, dt=%0.6f, alpha=%0.6f, beta=%0.6f' % (k, last_x, last_v, step_duration, dt, a, b)
+            waveform[k] = states[0, 0]
 
-            last_x = states[steps_per_step, 0]
-            last_v = states[steps_per_step, 1]
+            last_x = states[0, 0]
+            last_v = states[0, 1]
             if np.isnan(last_x):
                 last_x = 0.0
             if np.isnan(last_v):
                 last_v = 0.0
-            last_index = ei
 
         #transform the waveform into a spectrogram
         nstd = 6.0
+        freq_spacing = 125.0
         increment = 1.0 / 1000.0
-        window_length = nstd / (2.0*np.pi*125.0)
+        window_length = nstd / (2.0*np.pi*freq_spacing)
         spec_t,spec_f,spec,rms = gaussian_stft(waveform, self.waveform_sample_rate, window_length, increment, nstd=nstd, min_freq=0.0, max_freq=10000.0)
         spec = np.abs(spec)
 
@@ -71,24 +87,68 @@ class Motogram(object):
             print 'k=%d, mu_t=%0.6f, sigma1_t=%0.6f, sigma2_t=%0.6f' % (k, mu_t, sigma1_t, sigma2_t)
 
             #create gaussian based on sigma2
-            mean2 = mu_t - spec_f
+            mean2 = (mu_t - spec_f)
             mean2[mean2 < 0.0] = 0.0
             gauss2 = mean2**2 / (2*sigma2_t**2)
 
             #create gaussian based on sigma1
-            mean1 = spec_f - mu_t
+            mean1 = (spec_f - mu_t)
             mean1[mean1 < 0.0] = 0.0
             gauss1 = mean1**2 / (2*sigma1_t**2)
 
             #create filter as combination of two gaussians
             filt = np.exp(-(gauss1 + gauss2))
-            print list(filt)
-            print ''
 
             #filter the spectrogram
-            spec_filt[:, k] = spec[:, k]*filt
+            sfilt = spec[:, k]*filt
 
-        return spec_filt
+            #compute normalization factors
+            spi = spec[:, k].sum()
+            ssi = sfilt.sum()
+
+            #normalize the filtered spectrogram
+            spec_filt[:, k] = sfilt / (spi*ssi)
+
+        return waveform,spec_filt
+
+    def plot(self):
+
+        waveform,sim_spec = self.simulate()
+
+        nsp = 5
+        if hasattr(self, 'hedi_spectrogram'):
+            nsp += 1
+
+        plt.subplot(nsp, 1, 1)
+        plt.plot(waveform, 'k-')
+        plt.axis('tight')
+
+        plt.subplot(nsp, 1, 2)
+        plt.imshow(sim_spec, interpolation='nearest', aspect='auto')
+        plt.axis('tight')
+
+        plt.subplot(nsp, 1, 3)
+        plt.plot(self.alpha, 'r-')
+        #plt.plot(self.beta, 'b-')
+        #plt.legend(['alpha', 'beta'])
+        plt.axis('tight')
+
+        plt.subplot(nsp, 1, 4)
+        plt.plot(self.mu, 'k-')
+        plt.legend(['mu'])
+        plt.axis('tight')
+
+        plt.subplot(nsp, 1, 5)
+        plt.plot(self.sigma1, 'g-')
+        plt.plot(self.sigma2, 'k-')
+        plt.legend(['sigma1', 'sigma2'])
+        plt.axis('tight')
+
+        if hasattr(self, 'hedi_spectrogram'):
+            plt.subplot(nsp, 1, 6)
+            plt.imshow(self.hedi_spectrogram, interpolation='nearest', aspect='auto')
+            plt.axis('tight')
+
 
 
 def motograms_from_file(file_name, wav_file_dir):
